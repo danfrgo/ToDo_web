f13cdb161a505039cbc0c58f5dd75195c76e0bbb
danfrgo <danielfr_goncalves@hotmail.com>
2020-12-27 21:11:25 +0000

    Desenvolvimento do Header

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import * as S from './styles';
+
+
+
+// Components
+import Header from '../../components/Header';
 
 function Home() {
-    return <h1>Home</h1>
+    return (
+    <S.Container>
+    <Header/>
+    </S.Container>
+
+    )
 }
 
 export default Home;
