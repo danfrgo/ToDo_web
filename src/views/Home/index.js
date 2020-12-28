a8f097673169ce7bb01cb5e888feacf17571f580
danfrgo <danielfr_goncalves@hotmail.com>
2020-12-28 14:40:07 +0000

    Containers das Tarefas

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -7,6 +7,7 @@ import * as S from './styles';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import FilterCard from '../../components/FilterCard';
+import TaskCard from '../../components/TaskCard';
 
 
 
@@ -41,6 +42,25 @@ function Home() {
         </button>
     </S.FilterArea>
 
+    <S.Title>
+        <h3>TAREFAS</h3>
+    </S.Title>
+
+
+
+    <S.Content>
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+    </S.Content>
+    
     <Footer/>
     </S.Container>
 
--- src/views/Home/styles.js
+++ src/views/Home/styles.js
@@ -18,3 +18,31 @@ export const FilterArea = styled.div  `
     }
     
 `
+
+export const Content = styled.div`
+width : 100%;
+display: flex; //para colocar as tarefas lado a lado
+flex-wrap: wrap; // para quebrar de linha para nao sair fora do ecra
+justify-content: center; // centrar container
+
+
+`
+
+export const Title = styled.div`
+width: 100%;
+border-bottom: 1px solid #20295F;
+//text-align: center;
+display:flex;
+justify-content: center;
+margin-bottom: 20px;
+
+h3{
+    color: #20295F;
+    position: relative;
+    top: 30px;
+    background: #FFF;
+    padding: 0 20px;
+
+
+
+`
